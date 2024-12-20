e60bf31a8927d01dff70118ee73a4c249b98a686
peter <jerelepeter06@gmail.com>
2024-12-20 03:36:28 -0400

    used audio example from juce
    display a standalone vst with hello world window

--- plugin/include/achilleus/PluginEditor.h
+++ plugin/include/achilleus/PluginEditor.h
@@ -2,21 +2,23 @@
 
 #include "PluginProcessor.h"
 
-namespace audio_plugin {
+namespace audio_plugin
+{
 
-class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor {
-public:
-  explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
-  ~AudioPluginAudioProcessorEditor() override;
+class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor
+{
+  public:
+    explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
+    ~AudioPluginAudioProcessorEditor() override;
 
-  void paint(juce::Graphics&) override;
-  void resized() override;
+    void paint(juce::Graphics &) override;
+    void resized() override;
 
-private:
-  // This reference is provided as a quick way for your editor to
-  // access the processor object that created it.
-  AudioPluginAudioProcessor& processorRef;
+  private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    AudioPluginAudioProcessor &processorRef;
 
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
-}  // namespace audio_plugin
+} // namespace audio_plugin
